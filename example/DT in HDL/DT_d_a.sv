e55a09c4c10eb6487377054a268d6b5f6ef6d494
franklin-tang <tangqijia333@gmail.com>
2024-08-29 15:09:48 +0100

    Code uploaded

---
+++ example/DT in HDL/DT_d_a.sv
@@ -0,0 +1,49 @@
+`timescale 1ns / 1ps 
+
+module DT_d_a (Va,Vb,Vc,Ia,Ib,Ic,cls);
+
+parameter N = 'd8;
+parameter C = 'd1;
+input logic[N-1:0] Va,Vb,Vc,Ia,Ib,Ic;
+output logic [C-1:0] cls;
+
+logic ns0,ns1,ns2,ns3,ns6,ns10,ns11,ns12,ns15;
+parameter logic[N-1:0] t0 = 8'd119;
+parameter logic[N-1:0] t1 = 8'd196;
+parameter logic[N-1:0] t2 = 8'd63;
+parameter logic[N-1:0] t3 = 8'd111;
+parameter logic[N-1:0] t6 = 8'd61;
+parameter logic[N-1:0] t10 = 8'd143;
+parameter logic[N-1:0] t11 = 8'd138;
+parameter logic[N-1:0] t12 = 8'd116;
+parameter logic[N-1:0] t15 = 8'd142;
+
+assign ns0 = (Ib < t0)? 1:0;
+assign ns1 = (Va < t1)? 1:0;
+assign ns2 = (Vb < t2)? 1:0;
+assign ns3 = (Ib < t3)? 1:0;
+assign ns6 = (Vc < t6)? 1:0;
+assign ns10 = (Ib < t10)? 1:0;
+assign ns11 = (Ia < t11)? 1:0;
+assign ns12 = (Ia < t12)? 1:0;
+assign ns15 = (Ia < t15)? 1:0;
+
+always_comb
+begin
+    casex({ns0,ns1,ns2,ns3,ns6,ns10,ns11,ns12,ns15})
+		9'b1111?????: cls = 'd1;
+		9'b1110?????: cls = 'd0;
+		9'b110?1????: cls = 'd1;
+		9'b110?0????: cls = 'd1;
+		9'b10???????: cls = 'd0;
+		9'b0????111?: cls = 'd1;
+		9'b0????110?: cls = 'd0;
+		9'b0????10?1: cls = 'd1;
+		9'b0????10?0: cls = 'd1;
+		9'b0????0???: cls = 'd1;
+		default: cls = '0;
+	endcase
+end
+
+endmodule
+
---
+++ tool/transfer_read.py
@@ -0,0 +1,98 @@
+import ast
+import sys
+import pandas as pd
+
+
+def readfile(filename):
+    # filename = "detectionDT.dot"
+    # filename = "classificationDT.dot"
+    with open(filename, "r") as file:
+        text = file.readlines()
+
+    # print(text)
+    nodes = pd.DataFrame(columns=['number', 'feature', 'threshold'])
+    chains = pd.DataFrame(columns=['previous', 'next'])
+    leaves = pd.DataFrame(columns=['number', 'class'])
+    benches = pd.DataFrame(columns=['node', 'true', 'false'])
+
+    for line in text[3:-1]:
+        # print(line)
+        sign_node = line.find('<=')   # the sign that only the node lies have
+        sign_chain = line.find('->')  # the sign that only the chain lies have
+        if sign_node != -1:           # for the node line , e.g. 4 [label="Ib <= 57.0\nsamples = 28\nvalue = [0, 28]"] ;
+            node_number = line.find('[')  # which is after "4" in the e.g.
+            # nodes.append(line[:node_number])
+            node_con = line.find('"')     # which is before "Ib" in the e.g.
+            node_th = line.find('\\')     # which is after "57.0" in the e.g.
+            new_row = pd.DataFrame({'number': [int(line[:node_number])],
+                                    'feature': [line[(node_con+1):sign_node]],
+                                    'threshold': [int(float(line[(sign_node+2):node_th])+1.0)]
+                                    })
+            nodes = pd.concat([nodes, new_row], ignore_index=True)
+
+        elif sign_chain != -1:    # for the chain line, e.g. 1 -> 2 ;
+            end = line.find('[')  # the first line has some extra descriptions
+            if end != -1:  # the other line
+                chain = line[:end]
+                # chain.append(line[:end])
+            else:          # the first line
+                end = line.find(';')
+                chain = line[:end]
+                # chain.append(line[:end])
+            # print(chain[:sign_chain], chain[(sign_chain + 2):])
+            new_row = pd.DataFrame({'previous': [int(chain[:sign_chain])],
+                                    'next': [int(chain[(sign_chain + 2):])]
+                                    })  # add a new row to the chains with separate values
+            chains = pd.concat([chains, new_row], ignore_index=True)
+
+        else:  # for the leaf line, e.g. 5 [label="samples = 1\nvalue = [0, 1]"] ;
+            leaf_number = line.find('[')
+            # leafs.append(line[:end])
+            class_start = line.find('[', (leaf_number+1))
+            class_end = line.find(']')
+            class_amounts = ast.literal_eval(line[class_start:(class_end + 1)])  # transfer str'[]' to list
+            class_number = class_amounts.index(max(class_amounts))  # get the max number of class
+            new_row = pd.DataFrame({'number': [int(line[:leaf_number])],
+                                    'class': [class_number]
+                                    })
+            leaves = pd.concat([leaves, new_row], ignore_index=True)
+
+    # transfer the chains to benches
+    chains_sorted = chains.sort_values(by='previous', ignore_index=True)  # get the sequence by the previous node number
+    for node_group in range(0, chains_sorted.shape[0], 2):  # in chains, every two nodes, which should be the same, are in group
+        node_a = chains_sorted.at[node_group, 'previous']   # get the two node number
+        node_b = chains_sorted.at[(node_group + 1), 'previous']
+        if node_a != node_b:  # check the similarity of the nodes
+            print("DT export structure error")
+            sys.exit(1)
+        leaf_a = chains_sorted.at[node_group, 'next']     # get the two leaves after a node
+        leaf_b = chains_sorted.at[(node_group+1), 'next']
+        # print(leaf_a, leaf_b)
+        if leaf_a < leaf_b:  # the smaller number of the leaf is the true state.
+            new_row = pd.DataFrame({'node': [node_a],
+                                    'true': [leaf_a],
+                                    'false': [leaf_b]
+                                    })
+        else:
+            new_row = pd.DataFrame({'node': [node_a],
+                                    'true': [leaf_b],
+                                    'false': [leaf_a]
+                                    })
+        benches = pd.concat([benches, new_row], ignore_index=True)
+
+    # print(leaves)
+    # print(chains)
+    # print(nodes)
+    # print(chains_sorted)
+    # print(benches)
+    return nodes, chains, leaves, benches
+
+
+# filename = "detectionDT.dot"
+# # filename = "classificationDT.dot"
+# nodes, chains, leaves, benches = readfile(filename)
+# print(leaves)
+# # print(chains)
+# # print(nodes)
+# # print(benches)
+
